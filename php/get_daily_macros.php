55d8e294624603c2071be6592583f1a36ab80941
tylersriver <tyler.w.sriver@eagles.oc.edu>
2017-04-10 20:52:35 -0500

    Use only php files
    Also added php file to update dailys

--- php/get_daily_macros.php
+++ php/get_daily_macros.php
@@ -16,11 +16,69 @@ if($conn->connect_error){
 }
 
 // -- Process Data and Insert
-$sql  = "SELECT * FROM dailyMacros";
+$sql  = "SELECT protein, fat, carbs FROM dailyMacros";
 try { // Execute insert
   $result = mysqli_query($conn, $sql);
 } catch (Exception $err) {
-  die("Insert Failed: ".$err->error());
+  die("Insert Failed: ".$err->getMessage());
 }
 
 // -- Pass to POST
+$daily = $result->fetch_row();
+$protein = $daily[0];
+$fat = $daily[1];
+$carbs = $daily[2];
+
+$conn->close();
+?>
+<header>
+    <title>Macro Tracker</title>
+</header>
+<body>
+<header>
+    <ul>
+        <li><a href="home.php" class="button">Home</a></li>
+        <li><a href="get_daily_macros.php" class="button">Daily Macros</a></li>
+    </ul>
+</header>
+<div>
+    <h2>Current Daily Macros</h2>
+    <p>Protein: <?php echo $protein ?> </p>
+    <p>Fat:  <?php echo $fat ?> </p>
+    <p>Carbohydrates:  <?php echo $carbs ?> </p>
+</div>
+<div>
+    <h2>Set Daily Macros</h2>
+    <form action="insert_daily_macros.php" method="post">
+        <table>
+            <tr>
+                <td valign="top">
+                    <label>Protein</label>
+                <td valign="top">
+                    <input type="text" name="protein">
+                </td>
+            </tr>
+            <tr>
+                <td valign="top">
+                    <label>Carbs</label>
+                <td valign="top">
+                    <input type="text" name="carbs">
+                </td>
+            </tr>
+            <tr>
+                <td valign="top">
+                    <label>Fat</label>
+                <td valign="top">
+                    <input type="text" name="fat">
+                </td>
+            </tr>
+            <tr>
+                <td style="text-align:center">
+                    <input type="submit" value="Insert">
+                </td>
+            </tr>
+        </table>
+    </form>
+</div>
+
+
--- php/insert_daily_macros.php
+++ php/insert_daily_macros.php
@@ -1,9 +1,9 @@
 <?php
-// Created by Tyler Sriver on 2/24/2017
 /**
- * This file is called by home.php
- * to insert the meal values into the
- * database and then redirects back to home
+ * Created by PhpStorm.
+ * User: tyler
+ * Date: 4/10/2017
+ * Time: 8:03 PM
  */
 
 // -- Get POST Variables
@@ -24,18 +24,18 @@ $conn = new mysqli($servername, $username, $password, $db);
 
 // Check Connection
 if($conn->connect_error){
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
-// -- Process Data and Insert
-$sql  = "INSERT INTO mealEntries (entryTime, protein, carbs, fat)
-          VALUES (NOW(), $protein, $carbs, $fat)";
+// Update Macros
+$sql = "UPDATE dailyMacros 
+          SET protein = $protein,  fat = $fat, carbs = $carbs";
+
 try { // Execute insert
-  mysqli_query($conn, $sql);
+    mysqli_query($conn, $sql);
 } catch (Exception $err) {
-  die("Insert Failed: ".$err->getMessage());
+    die("Insert Failed: ".$err->getMessage());
 } // End SQL
 
 $conn->close();
-header( "Location: http://192.168.1.76/MacroTrackerWebsite/php/home.php" );
-exit();
+header( "Location: http://192.168.1.76/MacroTrackerWebsite/php/get_daily_macros.php" );
