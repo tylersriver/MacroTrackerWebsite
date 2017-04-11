8cc44e1be22723ac99591963ad99969e1861de4d
tylersriver <tyler.w.sriver@eagles.oc.edu>
2017-04-11 01:59:33 -0500

    Fix SUM select statements

--- php/home.php
+++ php/home.php
@@ -22,19 +22,38 @@
 include_once ("lib/MySQL_Tool.php");
 $conn = new MySQL_Tool();
 
-// Pull and Store Daily Values
+// Pull and Store Daily Values in variables
 $sql  = "SELECT protein, fat, carbs FROM dailyMacros";
 $result = $conn->executeSelect($sql);
-
-// -- Store Values
 $daily = $result->fetch_row();
 $proteinDaily = $daily[0];
 $fatDaily = $daily[1];
 $carbsDaily = $daily[2];
 
-$conn ->closeConn();
+// Get Day Totals
+$protein_result = mysqli_fetch_row($conn->executeSelect("SELECT SUM(m.protein) FROM mealEntries m WHERE DATE(entryTime) = DATE(NOW())"));
+$protein_day_sum = $protein_result[0];
+
+$fat_result = mysqli_fetch_row($conn->executeSelect("SELECT SUM(m.fat) FROM mealEntries m WHERE DATE(entryTime) = DATE(NOW())"));
+$fat_day_sum = $fat_result[0];
+
+$carbs_result = mysqli_fetch_row($conn->executeSelect("SELECT SUM(m.carbs) FROM mealEntries m WHERE DATE(entryTime) = DATE(NOW())"));
+$carbs_day_sum = $carbs_result[0];
+
+// Calculate remaining
+$protein_remaining = $proteinDaily - $protein_day_sum;
+$fat_remaining = $fatDaily - $fat_day_sum;
+$carbs_remaining = $carbsDaily - $carbs_day_sum;
+
 ?>
 <div>
+    <h2>Current Macros Left</h2>
+    <p>Protein: <?php echo $protein_remaining ?> </p>
+    <p>Fat:  <?php echo $fat_remaining ?> </p>
+    <p>Carbohydrates:  <?php echo $carbs_remaining ?> </p>
+</div>
+<div>
+    <h2>Insert Meal</h2>
     <form action="../php/insert_macros.php" method="post">
         <table>
             <tr>
